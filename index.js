12a8a16bfa434512fc51eaf14ee3fa106d3e3dd6
Nikhil Sinha <nikhilsinha198@gmail.com>
2026-05-08 20:11:06 +0530

    phase 1 done- initialize node.js project

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+
+  res.json({
+    message: "Hello World"
+  })
+
+})
+app.get("/health", (req, res) => {
+
+  res.json({
+    message: "Server is running and healthy"
+  })
+
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
