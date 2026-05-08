c255118b4beb61fcbad2707983069cd6cedd1c06
Nikhil Sinha <nikhilsinha198@gmail.com>
2026-05-08 23:35:55 +0530

    added docekr compose file

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-
   res.json({
-    message: "Hello World"
-  })
-
-})
+    message: "Hello World",
+  });
+});
 app.get("/health", (req, res) => {
-
   res.json({
-    message: "Server is running and healthy"
-  })
-
-})
-
+    message: "Server is running and healthy",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+})
